4c1424b99af582caa44b24c148b4a6598b9730d0
IzumiSy <beetle-noise@gmx.com>
2022-10-17 18:08:05 +0900

    Removed placeholder comments

--- graph/resolver.go
+++ graph/resolver.go
@@ -2,10 +2,6 @@
 
 package graph
 
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require here.
-
 import (
 	"database/sql"
 	"gqlgen-app/graph/model"
