187275561e0d937d2e6ff0e822198949cd3e0c30
IzumiSy <beetle-noise@gmx.com>
2022-10-19 17:32:57 +0900

    Defined relaion between todos and users

--- ent/schema/todo.go
+++ ent/schema/todo.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
 
 // Todo holds the schema definition for the Todo entity.
 type Todo struct {
@@ -9,10 +14,18 @@ type Todo struct {
 
 // Fields of the Todo.
 func (Todo) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Default(uuid.New),
+		field.String("text").NotEmpty(),
+		field.Bool("done").Default(false),
+	}
 }
 
 // Edges of the Todo.
 func (Todo) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.From("assignee", User.Type).
+			Ref("todos").
+			Unique(),
+	}
 }
