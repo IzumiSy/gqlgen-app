2d5b1474527da9ffee23ff84d67dc0fb45ec5b32
IzumiSy <beetle-noise@gmx.com>
2022-10-20 01:30:02 +0900

    Remove an old DDL file

--- initial.sql
+++
@@ -1,5 +0,0 @@
-drop table if exists user;
-drop table if exists todo;
-
-create table user(id text, name text);
-create table todo(id text, text text, done integer, user_id text);
--- initialize_db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sqlite3 main.db < initial.sql
