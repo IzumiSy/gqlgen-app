cfb01d85a594b6efc376905250d98b0d6c321555
IzumiSy <beetle-noise@gmx.com>
2022-10-21 17:52:27 +0900

    make ent

--- ent/migrate/schema.go
+++ ent/migrate/schema.go
@@ -10,7 +10,7 @@ import (
 var (
 	// CategoriesColumns holds the columns for the "categories" table.
 	CategoriesColumns = []*schema.Column{
-		{Name: "id", Type: field.TypeUUID},
+		{Name: "id", Type: field.TypeUUID, Unique: true},
 		{Name: "name", Type: field.TypeString},
 		{Name: "created_at", Type: field.TypeTime},
 	}
@@ -22,7 +22,7 @@ var (
 	}
 	// TodosColumns holds the columns for the "todos" table.
 	TodosColumns = []*schema.Column{
-		{Name: "id", Type: field.TypeUUID},
+		{Name: "id", Type: field.TypeUUID, Unique: true},
 		{Name: "text", Type: field.TypeString},
 		{Name: "done", Type: field.TypeBool, Default: false},
 		{Name: "updated_at", Type: field.TypeTime},
@@ -45,7 +45,7 @@ var (
 	}
 	// UsersColumns holds the columns for the "users" table.
 	UsersColumns = []*schema.Column{
-		{Name: "id", Type: field.TypeUUID},
+		{Name: "id", Type: field.TypeUUID, Unique: true},
 		{Name: "name", Type: field.TypeString},
 		{Name: "created_at", Type: field.TypeTime},
 	}
