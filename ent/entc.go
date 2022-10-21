576e032b0fed386c23c493f4c42ff46ae64e9d56
IzumiSy <beetle-noise@gmx.com>
2022-10-21 15:29:17 +0900

    Added codegen for GraqhQL integration

---
+++ ent/entc.go
@@ -0,0 +1,28 @@
+//go:build ignore
+
+package main
+
+import (
+	"log"
+
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+)
+
+func main() {
+	ex, err := entgql.NewExtension(
+		entqql.WithSchemaGenerator(),
+		entgql.WithSchemaPath("ent.graphql"),
+	)
+	if err != nil {
+		log.Fatalf("failed creating entgql extension: %s", err.Error())
+	}
+
+	opts := []entc.Option{
+		entc.Extensions(ex),
+	}
+	if err := entc.Generate("./ent/schema", &gen.Config{}, opts...); err != nil {
+		log.Fatalf("Failed running ent codegen: %s", err.Error())
+	}
+}
