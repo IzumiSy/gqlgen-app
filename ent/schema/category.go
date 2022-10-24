4d904cc32c6b8eb504fc70e58767c04466dc6da5
IzumiSy <beetle-noise@gmx.com>
2022-10-24 10:04:44 +0900

    Use mixin.CreateTime in Category schema

--- ent/schema/category.go
+++ ent/schema/category.go
@@ -1,13 +1,12 @@
 package schema
 
 import (
-	"time"
-
 	"entgo.io/contrib/entgql"
 	"entgo.io/ent"
 	"entgo.io/ent/schema"
 	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
 	"github.com/google/uuid"
 )
 
@@ -16,12 +15,17 @@ type Category struct {
 	ent.Schema
 }
 
+func (Category) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		mixin.CreateTime{},
+	}
+}
+
 // Fields of the Category.
 func (Category) Fields() []ent.Field {
 	return []ent.Field{
 		field.UUID("id", uuid.UUID{}).Default(uuid.New).Unique().Immutable(),
 		field.String("name").NotEmpty(),
-		field.Time("created_at").Default(time.Now).Immutable(),
 	}
 }
 
